5cd8729d1367093ac13f9af56ebc9e62a112139f
mohammed handi <mohammedh.hanmdi@gmail,com>
2026-07-15 11:07:32 +0300

    Update Google Maps iframe width in Contact page for improved responsiveness

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -254,7 +254,7 @@ export const Contact = () => {
         >
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d211.48708922356414!2d35.859807721235185!3d31.99361489137771!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca1682f3f48ef%3A0x7a9f5e1f0278ee59!2sTrustech%20Limited%20LLC!5e0!3m2!1sen!2sjo!4v1784099734509!5m2!1sen!2sjo"
-            width="600"
+            width="1200"
             height="450"
             style={{ border: 0 }}
             allowFullScreen={true}
